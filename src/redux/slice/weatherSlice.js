2ebfe8267f594efc091247c3e1e0282905aadbd1
DevaWinner <winneraaniekan@gmail.com>
2023-08-09 05:06:30 +0100

    Add weather slice and store in redux

---
+++ src/redux/slice/weatherSlice.js
@@ -0,0 +1,66 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { v4 as uuidv4 } from 'uuid';
+import { getCityData, getAirPollutionData } from '../../components/Api/api';
+
+const initialState = {
+  cityList: [
+    'London',
+    'New York',
+    'Tokyo',
+    'Prague',
+    'Berlin',
+    'Copenhagen',
+    'Madrid',
+    'Buenos Aires',
+    'Dubai',
+    'Rome',
+    'Marrakesh',
+    'Glasgow',
+    'Las Vegas',
+    'Beijing',
+    'Helsinki',
+    'Paris',
+  ],
+  dataOfCities: [],
+  isDataFetched: false,
+};
+
+const fetchCitiesWeatherData = createAsyncThunk(
+  'weather/fetchCitiesWeatherData',
+  async (_, { getState }) => {
+    const { cityList } = getState().weather;
+    const dataWeatherWithCities = await Promise.all(
+      cityList.map(async (city) => {
+        const cityData = await getCityData(city);
+        if (cityData.length > 0) {
+          const { lat, lon } = cityData[0];
+          const weatherData = await getAirPollutionData(lat, lon);
+          return {
+            id: uuidv4(),
+            city,
+            lat,
+            lon,
+            data: weatherData.list[0],
+          };
+        }
+        return {};
+      }),
+    );
+    return dataWeatherWithCities;
+  },
+);
+
+const weatherSlice = createSlice({
+  name: 'weather',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchCitiesWeatherData.fulfilled, (state, action) => {
+      state.dataOfCities = action.payload;
+      state.isDataFetched = true;
+    });
+  },
+});
+
+export default weatherSlice.reducer;
+export { fetchCitiesWeatherData };
