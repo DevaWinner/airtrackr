7c4f52c4e7f34871face640899e63bdd4ac072da
DevaWinner <winneraaniekan@gmail.com>
2023-08-09 05:07:22 +0100

    Stage app in APp.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Weather from './components/Weather';
+import WeatherDetails from './components/WeatherDetails';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Routes>
+      <Route path="/" element={<Weather />} />
+      <Route
+        path="/city/:id"
+        element={<WeatherDetails />}
+      />
+    </Routes>
+
   );
 }
 
