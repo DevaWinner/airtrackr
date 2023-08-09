ac3cee5621e6815067e8483b331d8ca4d1927f62
DevaWinner <winneraaniekan@gmail.com>
2023-08-09 09:33:04 +0100

    Restructure component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,6 +5,7 @@ import { fetchCitiesWeatherData } from '../redux/slice/weatherSlice';
 import Nav from './Nav';
 import CityWeather from './City/CityWeather';
 import './styles/Weather.css';
+import Africa from '../assets/africa.png';
 
 const Weather = () => {
   const [filteredCities, setFilteredCities] = useState([]);
@@ -31,16 +32,31 @@ const Weather = () => {
   return (
     <>
       <Nav onFilterChange={handleFilterChange} />
+
+      <div className="headline flex">
+        <div className="africa">
+          <img src={Africa} alt="Map of Africa" />
+        </div>
+        <>
+          {filteredCities.length > 0 && (
+          <div className="headline-city" to={`city/${filteredCities[0].id}`} key={filteredCities[0].id}>
+            <CityWeather city={filteredCities[0].city} aqi={filteredCities[0].data.main.aqi} />
+          </div>
+          )}
+        </>
+      </div>
+
       <div className="report-head">
         <p>City Reports</p>
       </div>
       <section className="main">
-        {filteredCities.map((city) => (
+        {filteredCities.slice(1).map((city) => (
           <Link className="city" to={`city/${city.id}`} key={city.id}>
             <CityWeather city={city.city} aqi={city.data.main.aqi} />
           </Link>
         ))}
       </section>
+
     </>
   );
 };
